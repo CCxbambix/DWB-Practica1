597925b0c250740f1a4a1bb8d2bb4c8b9e1dbeb2
CCxbambix <luisalbertosalazar@ciencias.unam.mx>
2026-09-01 15:49:25 -0600

    Documentacion de las clases terminada

--- prueba/CategoryService.java
+++ prueba/CategoryService.java
@@ -6,9 +6,26 @@ import java.util.List;
 import java.util.Map;
 
 public class CategoryService {
+
+    /**
+     * Atributos
+     */
+
+    //HashMap con clve un Integer ligado a la categoria correspondiente 
     private Map<Integer, Category> categoryMap = new HashMap<>();
+    //int con valor para el categoryId de la proxima categoria a crear
     private int globalIdCounter = 1;
 
+    /**
+     * Metodos 
+     */
+
+    /**
+     * Verifica que una categoria pueda ser creada correctamente, le asigan un id unico y la agrega al hashamp de categorias
+     * despues imprime un mensaje de exito, en caso de incumplir alguna condicion de creacion imprime un mensaje de 
+     * error y no lo registra
+     * @param Categoria creada por el usuario
+     */
     public void createCategory(Category newCategory) {
         // Validar que la categoría no sea su propio padre antes de cualquier operación
         if (newCategory.getParentCategoryId() != null && newCategory.getParentCategoryId().equals(globalIdCounter)) {
@@ -41,10 +58,17 @@ public class CategoryService {
         Integer newId = globalIdCounter++;
         newCategory.setCategoryId(newId);
         
+        //registra en el HashMap la categoria creada
         categoryMap.put(newId, newCategory);
+        //imprime mensaje de exito
         System.out.println("Categoría registrada correctamente: " + newCategory);
     }
 
+    /**
+     * Metodo que obtiene la lista de todas las categorias creadas hasta el momento y que tienen un estattus activo,
+     * para despues imprimir en pantalla todas las categorias, en caso de que no haya ninguna categoria activa o creada
+     * imprime un mensaje que diciendo que no hay categorias.
+     */
     public void getCategories() {
         List<Category> activeCategories = new ArrayList<>();
         for (Category c : categoryMap.values()) {
@@ -54,12 +78,19 @@ public class CategoryService {
         }
 
         if (activeCategories.isEmpty()) {
+            //Caso no hay categorias por mostrar
             System.out.println("No existen categorías registradas");
         } else {
+            //caso si hya al menos 1 categoria que mostrar
             System.out.println(activeCategories);
         }
     }
 
+    /**
+     * Obtiene todas las categorias hijas de la categoria pasada como parametro e imprime en pantalla
+     * una lista de los hijos, en caso de no tener hijos imprimira una lista vacia
+     * @param Category a la cual queremos obtener sus hijos
+     */
     public void getChildCategories(Integer categoryId) {
         List<Category> children = new ArrayList<>();
         for (Category c : categoryMap.values()) {
@@ -67,12 +98,20 @@ public class CategoryService {
                 children.add(c);
             }
         }
+        //imprime los hijos de la categoria
         System.out.println(children);
     }
 
+    /**
+     * Obtiene una categoria a la cual cambiara su estatus a 0, siendo 0 que esta categoria
+     * ya no esta activa, si la categoria tiene hijos imprimira un mensaje de error y no se cambiara el status
+     * de la categoria
+     * @param Id de la categoria a borrar
+     */
     public void deleteCategory(Integer id) {
         Category target = categoryMap.get(id);
 
+        //buscamos que si exista la categoria 
         if (target == null || target.getStatus() == 0) {
             System.out.println("Error: La categoría con ID " + id + " no existe o ya fue eliminada.");
             return;
@@ -85,8 +124,9 @@ public class CategoryService {
                 return;
             }
         }
-
+        //cambiamos el status a 0
         target.setStatus(0);
+        //imprimimos mensaje de exito
         System.out.println("Categoría eliminada con éxito.");
     }
 }
--- prueba/Main.java
+++ prueba/Main.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        /**
+         * Atributos
+         */
+        //Instancia de la clase CAtegoryServices para el uso de metodos de nuestras categorias
         CategoryService service = new CategoryService();
+        //Instancia de la clase scanner
         Scanner scanner = new Scanner(System.in);
+        //Atributo booleano para poder detener el programa
         boolean running = true;
 
         while (running) {
@@ -17,41 +24,50 @@ public class Main {
             System.out.println("5. Salir");
             System.out.print("Selecciona una opción: ");
 
+            //recibimos entrada del usuario
             String input = scanner.nextLine();
 
             switch (input) {
+                //Caso 1, Registramos una nueva categoria
                 case "1":
+                
                     System.out.print("Ingrese el nombre de la categoría: ");
+                    //Recibimos entrada del usuario para el nombre de la actegoria
                     String categoryStr = scanner.nextLine().trim();
 
                     System.out.print("Ingrese el tag de la categoría: ");
+                    //Recibimos entrada del usuario para el tag
                     String tagStr = scanner.nextLine().trim();
-
+                    //Verificamos que la entrada del usuario no sea vacia
                     if (categoryStr.isEmpty() || tagStr.isEmpty()) {
                         System.out.println("Error: El nombre de la categoría y el tag no pueden estar vacíos.");
                         break;
                     }
-
+    
                     System.out.print("Ingrese el ID del padre (presione ENTER si no tiene padre): ");
+                    //Recibimos la entrada del usuario  para ver si la actegoria es hijo o no de un categoria
                     String parentInput = scanner.nextLine().trim();
                     Integer parentId = null;
 
                     try {
+                        //If para cambiar el valor del parent Id por la entrada del usario
                         if (!parentInput.isEmpty()) {
                             parentId = Integer.parseInt(parentInput);
                         }
                         
                         Category newCategory = new Category(categoryStr, tagStr, parentId);
+                        //Comprobamos que la nueva categoria sea valida 
                         service.createCategory(newCategory);
                     } catch (NumberFormatException e) {
+                        //la entrada del Id del padre es invalida
                         System.out.println("Error: El ID del padre debe ser un número entero válido o vacío.");
                     }
                     break;
-
+                //Caso 2, Imprimimos las categorias activas
                 case "2":
                     service.getCategories();
                     break;
-
+                //Caso 3, imprimimos los hijos de una categoria 
                 case "3":
                     System.out.print("Ingrese el ID de la categoría padre: ");
                     try {
@@ -61,7 +77,7 @@ public class Main {
                         System.out.println("Error: Debe ingresar un valor numérico válido.");
                     }
                     break;
-
+                //Caso 4, Cmabiamos el status de una categoria
                 case "4":
                     System.out.print("Ingrese el ID de la categoría a eliminar: ");
                     try {
@@ -71,7 +87,7 @@ public class Main {
                         System.out.println("Error: Debe ingresar un valor numérico válido.");
                     }
                     break;
-
+                //caso5, terminamos el programa
                 case "5":
                     running = false;
                     System.out.println("Saliendo del programa...");
